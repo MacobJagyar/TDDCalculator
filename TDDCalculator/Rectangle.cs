f4a94969496dfdee2342fa22b56d0b5bb379286e
Eddy <eduardo.di.garcia@gmail.com>
2022-08-24 21:42:18 -0400

    Added Rectangle Method and Test

---
+++ TDDCalculator/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace TDDCalculator
+{
+    public static class Rectangle
+    {
+        public static decimal Length { get; set; }
+        public static decimal Width { get; set; }
+
+
+
+        public static decimal CalculateRectangleArea(decimal length,decimal width)
+        {
+
+            return length * width;
+        }
+
+        public static decimal CalculateRectanglePerimeter(decimal length, decimal width)
+        {
+
+            return 2*(length + width);
+        }
+
+    }
+}
+
---
+++ TDDCalculatorTests/RectangleTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TDDCalculator;
+
+namespace TDDCalculatorTests
+{
+    public class RectangleTests
+    {
+        [Theory]
+        [InlineData(10, 2, 20)]
+        [InlineData(10, 5, 50)]
+        public void CalculateRectangleArea_CorrectlyReturnsArea(decimal length, decimal width, decimal expected)
+        {
+            //Arrange
+
+            //Act
+            var results = Rectangle.CalculateRectangleArea(length, width);
+
+            //Assert
+            Assert.Equal(expected, results);
+
+        }
+
+        [Theory]
+        [InlineData(10, 2, 24)]
+        [InlineData(10, 5, 30)]
+        public void CalculateRectanglePerimeter_CorrectlyReturnsPerimeter(decimal length, decimal width, decimal expected)
+        {
+            //Arrange
+
+            //Act
+            var results = Rectangle.CalculateRectanglePerimeter(length, width);
+
+            //Assert
+            Assert.Equal(expected, results);
+
+        }
+
+    }
+}
+
